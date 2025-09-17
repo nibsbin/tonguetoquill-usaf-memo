3b5146b1639200b68225966780c529ba3d71dea1
nibs <jpthek9@gmail.com>
2025-09-16 20:03:49 -0400

    Remove content-guide and test-indorsements

--- build.sh
+++ build.sh
@@ -4,11 +4,6 @@
 
 echo "Compiling templates and tests to PDFs..."
 typst compile --font-path . template/starkindustries.typ pdfs/starkindustries.pdf
-typst compile --font-path . tests/test-indorsements.typ tests/test-indorsements.pdf
-
-
-# These template use a third party font included in the repository
-typst compile --font-path . template/content-guide.typ pdfs/content-guide.pdf
 typst compile --font-path . template/usaf-template.typ pdfs/usaf-template.pdf
 typst compile --font-path . template/ussf-template.typ pdfs/ussf-template.pdf
 
